f363717dfe821de771befab316c5bd2d6bf702ca
hyemmie <lhm0710@naver.com>
2023-12-13 00:19:13 +0900

    update config

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const withNextra = require("nextra")({
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 });
 
-const debug = process.env.NODE_ENV !== "production";
-const repository = "hyemmie.github.io";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // any configs you need
