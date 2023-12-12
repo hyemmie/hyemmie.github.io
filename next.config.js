a81425bea85eb28cefc3abc9852860c53e893ed1
hyemmie <lhm0710@naver.com>
2023-12-13 00:15:22 +0900

    update projects

--- next.config.js
+++ next.config.js
@@ -9,9 +9,6 @@ const repository = "hyemmie.github.io";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
   // any configs you need
 };
 
