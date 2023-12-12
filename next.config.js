8a8bab242933cd31ce3f58b46170a05390e9d8e1
hyemmie <lhm0710@naver.com>
2023-12-12 23:49:03 +0900

    update package.json and next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-blog',
-  themeConfig: './theme.config.js',
+const withNextra = require("nextra")({
+  theme: "nextra-theme-blog",
+  themeConfig: "./theme.config.js",
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
-})
+});
+
+const debug = process.env.NODE_ENV !== "production";
+const repository = "hyemmie.github.io";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
   // any configs you need
-}
+};
 
-module.exports = withNextra(nextConfig)
+module.exports = withNextra(nextConfig);
